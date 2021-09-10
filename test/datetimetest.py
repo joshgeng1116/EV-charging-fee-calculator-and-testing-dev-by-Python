daba5a1e26d79ffbcb0622c83310ec1ad4b5ac7d
Jeremy Chrimes <jeremy.chrimes@gmail.com>
2021-09-10 18:10:33 +1000

    Start working on devloping the first round of for REQ-1

---
+++ test/datetimetest.py
@@ -0,0 +1,15 @@
+"""
+FIT2107 2021 Semester 2 - NullPointerException
+
+Date First Modified: 2021-09-10
+Date Modified: 2021-00-10
+
+A test class to test REQ-1. 
+"""
+from app.calculator import *
+import unittest
+
+class TestDateTime(unittest.TestCase):
+    def test_working(self):
+        Calculator()
+        self.assertEqual()
