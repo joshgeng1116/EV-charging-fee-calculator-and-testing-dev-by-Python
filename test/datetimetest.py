c562d6a662c9bd11b23a7d6da4ba850c1c5ed3c0
Jeremy Chrimes <jeremy.chrimes@gmail.com>
2021-09-12 16:57:48 +1000

    Creation of test case for peak non-holiday

--- test/datetimetest.py
+++ test/datetimetest.py
@@ -10,10 +10,11 @@ from app.calculator import *
 import unittest
 
 class TestDateTime(unittest.TestCase):
-    # Test Weekday daytime (T000)
-    def test_working(self):
-        Calculator()
-        self.assertEqual()
+    # Test peak non-holiday (T000)
+    def test_peak_non_holiday(self):
+        calc = Calculator()
+        self.assertEqual(calc.time_calculation(20, 75, 80, 22), 2)
+        self.assertAlmostEqual(calc.cost_calculation(20,75,80,True, False), 6.60)
     
     # Test Weekday nighttime (T404)
 
@@ -28,5 +29,3 @@ class TestDateTime(unittest.TestCase):
     # Test weekend day and night 
 
     # test weekend night start and day stop 
-
-    
