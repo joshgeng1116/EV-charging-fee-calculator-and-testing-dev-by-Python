cbafb43924b70a320cb627a0b2bbc68985a4622c
Richard Yang <ylt096@gmail.com>
2021-09-12 17:06:29 +1000

    Modify code for T404 Test Weekday night

--- test/datetimetest.py
+++ test/datetimetest.py
@@ -10,23 +10,28 @@ from app.calculator import *
 import unittest
 
 class TestDateTime(unittest.TestCase):
+
     # Test Weekday daytime (T000)
     def test_working(self):
         Calculator()
         self.assertEqual()
     
     # Test Weekday nighttime (T404)
+    def test_weekday_night(self):
+        calc = Calculator()
+        self.assertEqual("2 Hours 0 Minute", calc.time_calculation(20, 75, 80, False, False))
+        self.assertEqual("$ 3.30", calc.cost_calculation(20, 75, 80, False, False))
 
-    # Test weekday day and night (T603)
+    # Test Weekday day and night (T603)
 
     # Test Weekday night and day (T702)
 
-    # Test Weekend daytime (T505)
+    # Test Weekend/Holiday daytime (T505)
 
-    # Test Weekday nighttime (T705)
+    # Test Weekend/Holiday nighttime (T705)
 
-    # Test weekend day and night 
+    # Test Weekend/Holiday day and night
 
-    # test weekend night start and day stop 
+    # Test Weekend/Holiday night start and day stop
 
     
