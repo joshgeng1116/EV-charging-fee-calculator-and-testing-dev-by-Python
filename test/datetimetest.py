0272a0405188790153dc3806be7bd667b0f416f1
Jeremy Chrimes <jchr0013@student.monash.edu>
2021-09-12 17:51:56 +1000

    Fix order of arguments in T404 assert statements.

--- test/datetimetest.py
+++ test/datetimetest.py
@@ -19,8 +19,8 @@ class TestDateTime(unittest.TestCase):
     # Test off-peak non-holiday (T404)
     def test_weekday_night(self):
         calc = Calculator()
-        self.assertEqual("2 Hours 0 Minute", calc.time_calculation(20, 75, 80, False, False))
-        self.assertEqual("$ 3.30", calc.cost_calculation(20, 75, 80, False, False))
+        self.assertEqual(calc.time_calculation(20, 75, 80, False, False), 2)
+        self.assertAlmostEqual(calc.cost_calculation(20, 75, 80, False, False), 3.30)
     # Test peak holiday(T505)
 
     # Test off-peak holiday (T705)
