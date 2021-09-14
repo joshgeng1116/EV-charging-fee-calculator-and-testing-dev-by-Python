a5d4698b7d94264cabbd3c4a2434958811fa7086
Jeremy Chrimes <jchr0013@student.monash.edu>
2021-09-14 22:22:14 +1000

    Add assertion to test cases and add testcases to gitlab-ci

--- test/timeconvertertest.py
+++ test/timeconvertertest.py
@@ -7,16 +7,16 @@ class TimeCoverterTest(unittest.TestCase):
 
     # Test when the time input is 23:59
     def test_time1(self):
-        time_to_minutes('23.59')
+        self.assertEqual(time_to_minutes('23:59'), 1439)
 
     # Test when the time input is 00:00
     def test_time2(self):
-        time_to_minutes('00:00')
+        self.assertEqual(time_to_minutes('00:00'), 0)
 
     # Test when the time input is 12:00
     def test_time3(self):
-        time_to_minutes('12:00')
+        self.assertEqual(time_to_minutes('12:00'), 720)
 
     # Test when the time input is 15:07
     def test_time4(self):
-        time_to_minutes('15:07')
+        self.assertEqual(time_to_minutes('15:07'), 907)
