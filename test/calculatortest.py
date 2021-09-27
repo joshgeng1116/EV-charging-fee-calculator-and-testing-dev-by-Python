5c5ee6e031f6d8ee8d238a2e57da3197ff6bd184
Jeremy Chrimes <jchr0013@student.monash.edu>
2021-09-27 17:16:05 +1000

    Integrate JouleupChargeConfigs into tests.
    Co-authored-by: jgen0005's avatarJingyun Geng <jgen0005@student.monash.edu>
    Co-authored-by: lyan0055's avatarLantao Yang <lyan0055@student.monash.edu>

--- test/calculatortest.py
+++ test/calculatortest.py
@@ -9,13 +9,14 @@ A test class to test REQ-1.
 
 from app.postcode import Postcode
 from app.calculator import Calculator
+from app.chargeconfig import JoulesupChargeConfigurations
 import unittest
 
 
 class CalculatorTest(unittest.TestCase):
     # Test peak non-holiday (T000)
     def test_peak_non_holiday(self):
-        calc = Calculator(20, 75, 80, 22, "12:00", "2021-08-20", Postcode("3800"))
+        calc = Calculator(20, 75, 80, JoulesupChargeConfigurations.LEVEL_5, "12:00", "2021-08-20", Postcode("3800"))
         self.assertAlmostEqual(calc.time_calculation(), 2)
         self.assertEqual(calc.get_duration_in_minutes(), 120)
         self.assertEqual(calc.get_minutes_in_peak_weekday(), 120)
@@ -26,7 +27,7 @@ class CalculatorTest(unittest.TestCase):
     
     # Test off-peak non-holiday (T404)
     def test_weekday_night(self):
-        calc = Calculator(20, 75, 80, 22, "18:00", "2021-08-20", Postcode("3800"))
+        calc = Calculator(20, 75, 80, JoulesupChargeConfigurations.LEVEL_5, "18:00", "2021-08-20", Postcode("3800"))
         self.assertEqual(calc.time_calculation(), 2)
         self.assertEqual(calc.get_duration_in_minutes(), 120)
         self.assertEqual(calc.get_minutes_in_peak_weekday(), 0)
@@ -37,7 +38,7 @@ class CalculatorTest(unittest.TestCase):
     
     # Test peak holiday(T505)
     def test_peak_holiday(self):
-        calc = Calculator(20, 75, 80, 22, "12:00", "2021-08-21", Postcode("3800"))
+        calc = Calculator(20, 75, 80, JoulesupChargeConfigurations.LEVEL_5, "12:00", "2021-08-21", Postcode("3800"))
         self.assertEqual(calc.time_calculation(), 2)
         self.assertEqual(calc.get_duration_in_minutes(), 120)
         self.assertEqual(calc.get_minutes_in_peak_weekday(), 0)
@@ -50,7 +51,7 @@ class CalculatorTest(unittest.TestCase):
     # Test off-peak holiday (T705)
 
     def test_off_peak_holiday(self):
-        calc = Calculator(50, 60, 50, 2, "20:00", "2021-08-22", Postcode("3800"))
+        calc = Calculator(50, 60, 50, JoulesupChargeConfigurations.LEVEL_1, "20:00", "2021-08-22", Postcode("3800"))
         self.assertAlmostEqual(calc.time_calculation(), 2.5)
         self.assertEqual(calc.get_duration_in_minutes(), 150)
         self.assertEqual(calc.get_minutes_in_peak_weekday(), 0)
