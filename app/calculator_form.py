93bdd4f268597b68207da3a5353cde59d988a3b9
Richard Yang <ylt096@gmail.com>
2021-10-01 21:53:26 +1000

    Update calculatorform function, delete repeat function(check data is blank or not) Build up calculatorformtest functiom

--- app/calculator_form.py
+++ app/calculator_form.py
@@ -31,65 +31,51 @@ class Calculator_Form(FlaskForm):
     # use validate_ + field_name to activate the flask-wtforms built-in validator
     # this is an example for you
     def validate_BatteryPackCapacity(self, field):
-        if field.data is None:
-            raise ValidationError('Field data is none')
-        elif field.data == '':
-            raise ValueError("cannot fetch data")
-        elif field.data < 0:
-            raise ValueError("Battery cannot have a negative capacity")
-        elif field.data == 0:
-            raise ValueError("Battery cannot have a capacity of zero")
         try:
-            float(field.data)
+            float(field)
         except:
             raise ValueError("Battery Capacity can only contain numbers > 0.")
+        if float(field) < 0:
+            raise ValueError("Battery cannot have a negative capacity")
+        elif float(field) == 0:
+            raise ValueError("Battery cannot have a capacity of zero")
 
     # validate initial charge here
     def validate_InitialCharge(self, field):
         # another example of how to compare initial charge with final charge
         # you may modify this part of the code
-        if field.data is None:
-            raise ValidationError("Initial state of battery not provided")
-        elif field.data == '':
-            raise ValueError("cannot fetch data")
-        elif field.data < 0:
-            raise ValueError("Initial state of battery is not vaild.")
-        elif field.data > self.FinalCharge.data:
-            raise ValueError("Initial charge data error")
-        elif field.data == 100:
-            raise ValueError("Battery cannot be charged since the Initial state of battery is full.")
-        elif field.data > 100:
-            raise ValueError("Invalid input, battery state cannot over 100%")
         try:
-            int(field.data)
+            int(field)
         except:
-            raise ValueError("Battery state can only contain 0 < numbers <= 100.")
+            raise ValueError("Battery state can only contain numbers from 0 to 100.")
+
+        if int(field) < 0:
+            raise ValueError("Initial state of battery is not vaild.")
+        elif int(field) > self.FinalCharge.data:
+            raise ValueError("Initial charge data error")
+        elif int(field) == 100:
+            raise ValueError("Battery cannot be charged since the Initial state of battery is full.")
+        elif int(field) > 100:
+            raise ValueError("Invalid input, battery state cannot over 100%")
 
     # validate final charge here
     def validate_FinalCharge(self, field):
-        if field.data is None:
-            raise ValidationError("Final state of battery not provided")
-        elif field.data == '':
-            raise ValueError("cannot fetch data")
-        elif field.data < 0:
-            raise ValueError("Final state of battery is not valid.")
-        elif field.data > self.FinalCharge.data:
-            raise ValueError("Final charge data error")
-        elif field.data == 100:
-            raise ValueError("Battery cannot be charged since the Final state of battery is full.")
-        elif field.data > 100:
-            raise ValueError("Invalid input, battery state cannot over 100%")
         try:
-            int(field.data)
+            int(field)
         except:
-            raise ValueError("Battery state can only contain 0 < numbers <= 100.")
+            raise ValueError("Battery state can only contain numbers from 0 to 100.")
+        if int(field) < 0:
+            raise ValueError("Final state of battery is not valid.")
+        elif int(field) > self.FinalCharge.data:
+            raise ValueError("Final charge data error")
+        elif int(field) == 100:
+            raise ValueError("Battery cannot be charged since the Final state of battery is full.")
+        elif int(field) > 100:
+            raise ValueError("Invalid input, battery state cannot over 100%")
+
 
     # validate start date here
     def validate_StartDate(self, field):
-        if field is None:
-            raise ValidationError('Start date is none')
-        elif field == '':
-            raise ValueError("cannot fetch start date")
         try:
             date.fromisoformat(field)
         except:
@@ -97,10 +83,6 @@ class Calculator_Form(FlaskForm):
 
     # validate start time here
     def validate_StartTime(self, field):
-        if field is None:
-            raise ValidationError('Start time is none')
-        elif field == '':
-            raise ValueError("cannot fetch start time")
         try:
             time.fromisoformat(field)
         except:
@@ -108,18 +90,13 @@ class Calculator_Form(FlaskForm):
 
     # validate charger configuration here
     def validate_ChargerConfiguration(self, field):
-        if field is None:
-            raise ValidationError("Charger configuration should not be None.")
-        else:
-            try:
-                JoulesupChargeConfigurations().get_config(field)
-            except InvalidConfigException:
-                raise ValueError("Invalid charging configuration")
+        try:
+            JoulesupChargeConfigurations().get_config(field)
+        except InvalidConfigException:
+            raise ValueError("Invalid charging configuration")
 
     # validate postcode here
     def validate_PostCode(self, field):
-        if field is None:
-            raise ValidationError("Post code should not be none")
         try:
             Postcode(field)
         except InvalidPostcodeException:
--- test/calculator_form_test.py
+++ test/calculator_form_test.py
@@ -1,20 +1,19 @@
 from app.calculator_form import Calculator_Form
-from app.chargeconfig import JoulesupChargeConfigurations
 import unittest
+from unittest.mock import Mock
 
 
 class Calculator_FormTest(unittest.TestCase):
-    def all_correct(self):
-        form = Calculator_Form(None, None, None, None, None, None, None)
-        self.assertEqual(form.BatteryPackCapacity(), 'Field data is none')
-        self.assertEqual(form.InitialCharge(), "Initial state of battery not provided")
-        self.assertEqual(form.FinalCharge(), "Final state of battery not provided")
-        self.assertEqual(form.StartDate(), 'Start date is none')
-        self.assertEqual(form.StartTime(), 'Start time is none')
-        self.assertEqual(form.ChargerConfiguration(), "Charger configuration should not be None.")
-        self.assertEqual(form.PostCode(), "Post code should not be none")
+
+    def test_all_none_input(self):
+        self.mock_Calculator_Form = Mock()
+        self.mock_Calculator_Form.side_effect = ValueError("Battery cannot have a negative capacity")
+        self.mock_Calculator_Form.validate_BatteryPackCapacity(self.mock_Calculator_Form, -1)
 
 
 def main():
     suite = unittest.TestLoader().loadTestsFromTestCase(Calculator_FormTest)
     unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+main()
