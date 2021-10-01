deef01e6b1c56adbdde5e468af51d35fba5cb107
Richard Yang <ylt096@gmail.com>
2021-10-01 21:58:30 +1000

    Update calculator form test with battery capacity finished

--- test/calculator_form_test.py
+++ test/calculator_form_test.py
@@ -5,10 +5,20 @@ from unittest.mock import Mock
 
 class Calculator_FormTest(unittest.TestCase):
 
-    def test_all_none_input(self):
+    def test_battery_capacity_negative(self):
         self.mock_Calculator_Form = Mock()
         self.mock_Calculator_Form.side_effect = ValueError("Battery cannot have a negative capacity")
-        self.mock_Calculator_Form.validate_BatteryPackCapacity(self.mock_Calculator_Form, -1)
+        self.mock_Calculator_Form.validate_BatteryPackCapacity(self.mock_Calculator_Form, "-1")
+
+    def test_battery_capacity_invalid(self):
+        self.mock_Calculator_Form = Mock()
+        self.mock_Calculator_Form.side_effect = ValueError("Battery Capacity can only contain numbers > 0.")
+        self.mock_Calculator_Form.validate_BatteryPackCapacity(self.mock_Calculator_Form, "abc")
+
+    def test_battery_capacity_zero(self):
+        self.mock_Calculator_Form = Mock()
+        self.mock_Calculator_Form.side_effect = ValueError("Battery cannot have a capacity of zero")
+        self.mock_Calculator_Form.validate_BatteryPackCapacity(self.mock_Calculator_Form, "0")
 
 
 def main():
