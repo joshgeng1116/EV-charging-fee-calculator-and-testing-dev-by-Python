9d5b2680819b68c5dc8926037984fdc1c039c0e9
Jeremy Chrimes <jchr0013@student.monash.edu>
2021-10-02 18:06:12 +1000

    Register tests lost with merge with __init__ file.

--- test/__init__.py
+++ test/__init__.py
@@ -5,3 +5,6 @@ from .postcodetest import *
 from .time_segments_test import *
 from .chargeconfigtest import *
 from .calculator_form_test import *
+from .energy_cal_test import * 
+from .weather_data_api_test import * 
+from .calculatorpresentationtest import *
